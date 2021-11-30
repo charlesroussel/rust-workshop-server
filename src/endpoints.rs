60a5bfae4b529882bc542d73ef3cc0123f90a0bb
Charles Roussel <charles.roussel@ogury.co>
2021-11-30 13:12:48 +0100

    add async endpoint

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,13 +1,16 @@
 
+use std::error::Error;
+
 use actix_web::{HttpResponse, Result, error::ResponseError, get, post, web::Json};
 use serde::{Serialize, Deserialize};
-use derive_more::{Display, Error};
+use derive_more::{Display};
 
-#[derive(Debug, Display, Error)]
-struct MyError {
-    value: &'static str
-}
+#[derive(Default, Debug, Display)]
+#[display(fmt="Error {}", self.0)]
+struct MyError(String);
 
+
+impl Error for MyError {}
 impl ResponseError for MyError {}
 
 #[derive(Deserialize, Default)]
@@ -38,9 +41,7 @@ async fn json_in(m: Json<MessageIn>) -> Result<String> {
     if let Some(f2) = &m.string_field {
         Ok(f2.clone())
     } else {
-        Err(MyError{
-            value: "Invalid JSON"
-        }.into())
+        Err(MyError( "Invalid JSON".into()).into())
     }
 }
 
@@ -71,6 +72,19 @@ async fn json_inout(m: Json<MessageInOut>) -> Result<Json<MessageInOut>> {
     Ok(m)
 }
 
+#[get("/basic_async")]
+async fn basic_async() -> Result<String> {
+    let endpoint = "https://httpbin.org/get";
+    let body = reqwest::get(endpoint)
+    .await
+    .map_err(|error| MyError(format!("Reqwest Error: {:?}", error)))?
+    .text()
+    .await
+    .map_err(|error| MyError(format!("Reqwest Error: {:?}", error)))?;
+
+    Ok(body)
+}
+
 
 
 
@@ -81,5 +95,6 @@ pub fn create_service() -> actix_web::Scope {
         .service(json_out)
         .service(json_out2)
         .service(json_inout)
+        .service(basic_async)
 
 }
